a9b76ba27adee9db4e11a5b11fdb0c965c62bcef
upitroma <55750919+upitroma@users.noreply.github.com>
2022-09-28 19:32:29 -0400

    more frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,21 +1,33 @@
 const title = document.getElementById('title');
-    const foundLog = document.getElementById('foundLog');
+const desc = document.getElementById('desc');
+const foundLog = document.getElementById('foundLog');
 
-    const urlParams = new URLSearchParams(location.search);
+const urlParams = new URLSearchParams(location.search);
 
-    duckID = urlParams.get('id');
+duckID = urlParams.get('id');
 
-    // call api on port 8080
-    fetch('http://localhost:8080/api?id=' + duckID)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            title.innerHTML = data.duckName;
-            log="";
-            for (i in data.foundLog){
-                date= new Date(data.foundLog[i].date)
-                date=date.getMonth()+"/"+date.getDate()+"/"+date.getFullYear()+" "+date.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3")
-                log += "<li>" + date + "</li>";
-            }
-            foundLog.innerHTML = log;
-    });
+// call api
+fetch('http://localhost:8080/api?id=' + duckID)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        //set title
+        title.innerHTML = "You found "+data.duckName+"!";
+
+        //set description
+        if(data.foundLog.length == 0){
+            desc.innerHTML = "You are the first person to find "+data.duckName+"!";
+        } else {
+            desc.innerHTML = data.duckName+" was found a total of " + data.foundLog.length + " times.";
+        }
+
+        //set log
+        log="";
+        for (i in data.foundLog){
+            date= new Date(data.foundLog[i].date)
+            date=date.getMonth()+"/"+date.getDate()+"/"+date.getFullYear()+" "+date.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3")
+            log += "<li>"+data.duckName+" was found at "+date+". </li>";
+        }
+        foundLog.innerHTML = log;
+});
