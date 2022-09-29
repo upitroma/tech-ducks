697bbde1f6894f3acbad8160f9bd8d9c06031257
upitroma <55750919+upitroma@users.noreply.github.com>
2022-09-28 20:02:13 -0400

    better frontend

--- frontend/index.js
+++ frontend/index.js
@@ -1,33 +1,50 @@
 const title = document.getElementById('title');
+const pageTitle = document.getElementById('pageTitle');
 const desc = document.getElementById('desc');
 const foundLog = document.getElementById('foundLog');
+const info = document.getElementById('info');
 
 const urlParams = new URLSearchParams(location.search);
 
 duckID = urlParams.get('id');
 
 // call api
+responseCode="";
+
+// TODO: non-hardcoded api url
 fetch('http://localhost:8080/api?id=' + duckID)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
+    .then(response => {
+        responseCode = response.status;
+        response.json().then(data => {
 
-        //set title
-        title.innerHTML = "You found "+data.duckName+"!";
+            console.log(data);
 
-        //set description
-        if(data.foundLog.length == 0){
-            desc.innerHTML = "You are the first person to find "+data.duckName+"!";
-        } else {
-            desc.innerHTML = data.duckName+" was found a total of " + data.foundLog.length + " times.";
-        }
+            if (responseCode == 200) {
+                info.hidden=false;
+            
+                //set title
+                title.innerHTML = "You found "+data.duckName+"!";
+                pageTitle.innerHTML = "You found "+data.duckName+"!";
 
-        //set log
-        log="";
-        for (i in data.foundLog){
-            date= new Date(data.foundLog[i].date)
-            date=date.getMonth()+"/"+date.getDate()+"/"+date.getFullYear()+" "+date.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3")
-            log += "<li>"+data.duckName+" was found at "+date+". </li>";
-        }
-        foundLog.innerHTML = log;
+                //set description
+                if(data.foundLog.length == 0){
+                    desc.innerHTML = "You are the first person to find "+data.duckName+"!";
+                } else {
+                    desc.innerHTML = data.duckName+" was found a total of " + data.foundLog.length + " times.";
+                }
+
+                //set log
+                log="";
+                for (i in data.foundLog){
+                    date= new Date(data.foundLog[i].date)
+                    date=date.getMonth()+"/"+date.getDate()+"/"+date.getFullYear()+" "+date.toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3")
+                    log += "<li>"+data.duckName+" was found at "+date+". </li>";
+                }
+                foundLog.innerHTML = log;
+            }
+            else{
+                title.innerHTML = "ERR: INVALID_DUCK";
+                pageTitle.innerHTML = "ERR: INVALID_DUCK";
+            }
+        });
 });
